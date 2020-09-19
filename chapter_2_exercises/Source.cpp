b57cff5c35cc6b5a0beb2d3884260ce69803d6e7
Yaroslav Kyrychenko <62503200+yaroslav-kyrychenko@users.noreply.github.com>
2020-09-19 12:39:18 +0200

    Adding the first exercise from the second chapter.

---
+++ chapter_2_exercises/Source.cpp
@@ -0,0 +1,13 @@
+// exercise 1 -- write a program, which displays a surname and address of the author 
+// (doesn't have to be a real one)
+
+#include <iostream>
+
+int main()
+{
+	using std::cout;
+	using std::endl;
+	cout << "The surname is Potter." << endl;
+	cout << "The address is Under the cupboard Staircase, 4 Privet Drive, Little Whinging, Surrey." << endl;
+	return 0;
+}
